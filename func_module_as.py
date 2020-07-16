c2098c26daf352dc487ae4d50a8ff1799d770248
unknown <yeonwoongchoi>
2020-07-16 12:40:59 +0900

    remain_date_input

--- func_module_as.py
+++ func_module_as.py
@@ -8,7 +8,19 @@ def date_now():
     return dt.datetime.now()
 
 def remain_date():
-    #print(dt,date.today())
+    # print(dt.date.today())
     today = dt.date.today()
     print('오늘은 {}년 {}월 {}일입니다'.format(today.year, today.month, today.day))
-    return (dt.datetime(2020,12,25) - dt.datetime.now())
+    # print(dt.datetime.now().replace(month=12 , day=25))
+    return dt.datetime(2020,12,25)-dt.datetime.now()
+
+def remain_date_input(nmonth, nday):
+    # print(dt.date.today())
+    today = dt.date.today()
+    print('오늘은 {}년 {}월 {}일입니다'.format(today.year, today.month, today.day))
+    # print(dt.datetime.now().replace(month=12 , day=25))
+    return dt.datetime(2020,nmonth,nday)-dt.datetime.now()
+
+nmonth = int(input('원하는 월을 입력하세요'))
+nday = int(input('원하는 일을 입력하세요'))
+print(remain_date_input(nmonth,nday))
