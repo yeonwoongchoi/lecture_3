8c75a5f45915d6ff74fd45d7dec90de47b772aa5
unknown <yeonwoongchoi>
2020-07-16 12:45:34 +0900

    app.py로 remain_date_input를 import해서 사용

--- func_module_as.py
+++ func_module_as.py
@@ -14,13 +14,15 @@ def remain_date():
     # print(dt.datetime.now().replace(month=12 , day=25))
     return dt.datetime(2020,12,25)-dt.datetime.now()
 
-def remain_date_input(nmonth, nday):
+def remain_date_input(nmonth = 12, nday = 11):
     # print(dt.date.today())
     today = dt.date.today()
     print('오늘은 {}년 {}월 {}일입니다'.format(today.year, today.month, today.day))
     # print(dt.datetime.now().replace(month=12 , day=25))
+    nmonth = int(input('원하는 월을 입력하세요'))
+    nday = int(input('원하는 일을 입력하세요'))
     return dt.datetime(2020,nmonth,nday)-dt.datetime.now()
 
-nmonth = int(input('원하는 월을 입력하세요'))
-nday = int(input('원하는 일을 입력하세요'))
+#nmonth = int(input('원하는 월을 입력하세요'))
+#nday = int(input('원하는 일을 입력하세요'))
 print(remain_date_input(nmonth,nday))
